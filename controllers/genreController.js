1e299a640572555e02530bb890da4f4b3d4e0e6c
Houser97 <arturo.riverar97@gmail.com>
2022-07-27 20:55:26 -0500

    Fill genre controllers GET and POST to create form

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,7 @@
 var Genre = require('../models/genre');
 let Book = require("../models/book");
 let async = require("async");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all Genre.
 exports.genre_list = function(req, res, next) {
@@ -40,12 +41,48 @@ exports.genre_detail = function(req, res, next) {
 
 // Display Genre create form on GET.
 exports.genre_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+    res.render('genre_form', {title: "Create Genre"});
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create POST');
+exports.genre_create_post = function(req, res, next) {
+    // Paso 1. Sanitizar y validar el campo de nombre.
+    body('name', 'Genre name required').trim().isLength({min: 1}).escape();
+    // Paso 2. Procesar solicitud.
+    (req, res, next) => {
+        //Paso 2.1. Atrapar errores
+        const errors = validationResult(req);
+
+        //Paso 2.2. Crear objeto GENRE con datos ESCAPED y TRIMMED.
+        const genre = new Genre(({name: req.body.name}));
+
+        //Paso 2.3. SI hay errores, volver a mostrar el formulario con los errores.
+        if(!errors.isEmpty()){
+            res.render("genre_form", {
+                title: "Create Genre",
+                genre, //Este es el objeto GENRE creado.
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            //Los datos son válidos
+            //Paso 2.4. Comprobar que el objeto creado no exista ya.
+            Genre.findOne({name: req.body.name}).exec(function(err, found_genre){
+                if(err){return next(err)}
+            })
+
+            if(found_genre){
+                res.redirect(found_genre.url);
+            } else {
+                //EN caso de que no exista el género, se guarda la instancia.
+                genre.save((err) => {
+                    if(err){return next(err)}
+                }) 
+                // Una vez que se guardó, se redirecciona al usuario al género creado.
+                res.redirect(genre.url);
+            }
+        }
+    }
 };
 
 // Display Genre delete form on GET.
