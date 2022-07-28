26494efa76f2939dbf91bd78468dc2fb6231f7ed
Houser97 <arturo.riverar97@gmail.com>
2022-07-28 09:09:22 -0500

    Create author form template and modify post and get controllers

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,7 @@
 let Author = require("../models/author");
 let Book = require("../models/book");
 let async = require("async");
+const { body, validationResult } = require('express-validator')
 
 // Mostrar lista de todos los autores.
 exports.author_list = function(req, res, next){
@@ -43,13 +44,49 @@ exports.author_detail = function(req, res, next){
 
 // Mostrar formulario de creación de autores con verbo GET.
 exports.author_create_get = function(req, res){
-    res.send("No implementado: Crear autor con GET.");
+    res.render('author_form', {
+        title: "Create Author"
+    });
 }
 
 // Gestionar creación de autor con POST.
-exports.author_create_post = function(req, res){
-    req.send("No implementado: Crear autor con POST");
-}
+exports.author_create_post = [
+    body("first_name", "Author first name required").trim().isLength({min: 1})
+                                                    .escape()
+                                                    .withMessage("First name must be specified.")
+                                                    .isAlphanumeric()
+                                                    .withMessage("First name has non-alphanumeric characters."),
+    body("family_name", "Author family name required").trim().isLength({min: 1})
+                                                      .escape()
+                                                      .withMessage("Family name must be specified.")
+                                                      .isAlphanumeric()
+                                                      .withMessage("Family name has non-alphanumeric characters."),
+    body("date_of_birth", "Invalid date of birth").optional({checkFalsy: true}).isISO8601().toDate(),
+    body("date_of_death", "Invalid date of death").optional({checkFalsy: true}).isISO8601().toDate(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            res.render("author_form", {
+                title: "Create Author",
+                author: req.body,
+                errors: errors.array()
+            });
+            return;
+        } else {
+            let author = new Author({
+                first_name: req.body.first_name,
+                family_name: req.body.family_name,
+                date_of_birth: req.body.date_of_birth,
+                date_of_death: req.body.date_of_death,
+            });
+            author.save(function(err) {
+                if(err){return next(err)}
+                res.redirect(author.url)
+            });
+        }
+    },
+];
 
 // Mostrar formulario para eliminar autor con GET
 exports.author_delete_get = function(req, res){
